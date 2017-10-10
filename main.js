02c3814303c491b66b0a0d96f03c76f8f1f2e9e5
TuulikkiLaine <tuulikki.laine2@gmail.com>
2017-10-10 10:10:17 +0300

    Service worker and manifest

--- main.js
+++ main.js
@@ -7,3 +7,8 @@ app.controller('sw-board', function($scope,$http) {
   $scope.probe = (planet) => planet.probed ? planet.probed = 0 : planet.probed = 1;
   $scope.explore = (planet) => planet.explored ? planet.explored = 0 : planet.explored = 1;
 });
+
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js');
+}
