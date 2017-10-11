2e3c17438a1b69be84603679322216f80c367a6d
TuulikkiLaine <tuulikki.laine2@gmail.com>
2017-10-11 13:56:02 +0300

    exemptions for Coruscant

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var app = angular.module('sw', []);
 app.controller('sw-board', function($scope,$http) {
   $http.get('data.json').then(function (res){
   $scope.regions = res.data;
+  $scope.regions[5].planets[3].probed = 1; //CORUSCANT
   });
   $scope.probe = (planet) => planet.probed ? planet.probed = 0 : planet.probed = 1;
   $scope.explore = (planet) => planet.explored ? planet.explored = 0 : planet.explored = 1;
