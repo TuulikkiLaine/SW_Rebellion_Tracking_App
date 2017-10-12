86fa3f31deccc198cd3c8162cd3de27113badaf6
TuulikkiLaine <tuulikki.laine2@gmail.com>
2017-10-12 09:18:21 +0300

    clear all button and possibility to probe by clicking map

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ app.controller('sw-board', function($scope,$http) {
   });
   $scope.probe = (planet) => planet.probed ? planet.probed = 0 : planet.probed = 1;
   $scope.explore = (planet) => planet.explored ? planet.explored = 0 : planet.explored = 1;
+  $scope.clear = (regions) => regions.map((region) => region.planets.map((planet) => planet.explored = 0))
 });
 
 
