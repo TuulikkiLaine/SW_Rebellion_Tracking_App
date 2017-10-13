3870204c849051529157b781add4d5326189f3ae
TuulikkiLaine <tuulikki.laine2@gmail.com>
2017-10-13 08:50:28 +0300

    map status fixed

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ app.controller('sw-board', function($scope,$http) {
   $http.get('data.json').then(function (res){
   $scope.regions = res.data;
   $scope.regions[5].planets[3].probed = 1; //CORUSCANT
+  $scope.regions[5].planets[3].explored = 1;
   });
   $scope.probe = (planet) => planet.probed ? planet.probed = 0 : planet.probed = 1;
   $scope.explore = (planet) => planet.explored ? planet.explored = 0 : planet.explored = 1;
